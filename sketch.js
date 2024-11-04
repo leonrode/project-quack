50190da24bc44744aa2195c485ee8885617114d3
Legitstorm <lsomerville8755@gmail.com>
2024-11-04 14:17:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+  noLoop(); // To stop draw() from looping
+}
+
+function draw() {
+  let gridSize = 8;
+  let cellSize = width / gridSize;
+
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      if ((i + j) % 2 == 0) {
+        fill(0); // Black color
+      } else {
+        fill(255); // White color
+      }
+      rect(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
+}
