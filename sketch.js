0dc008cd72ad2d6b7921e12a024cc551bca4458b
Leon Rode <leon.rode13@gmail.com>
2024-11-04 09:37:40 -0500

    recreated project using p5.vscode extension as per p5js docs

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 function setup() {
-    const SIZE = 800;
-    createCanvas(SIZE, SIZE);
-    background(0);
+  const SIZE = 800;
+  createCanvas(SIZE, SIZE);
+  background(0);
 
-    const NUM_TILES = 8;
+  const NUM_TILES = 16;
 
-    for (let i = 0; i < NUM_TILES; i++) {
-        for (let j = 0; j < NUM_TILES; j++) {
-            const x = (SIZE / NUM_TILES) * i;
-            const y = (SIZE / NUM_TILES) * j;
+  for (let i = 0; i < NUM_TILES; i++) {
+      for (let j = 0; j < NUM_TILES; j++) {
+          const x = (SIZE / NUM_TILES) * i;
+          const y = (SIZE / NUM_TILES) * j;
 
-            fill((i + j) % 2 * 255);
-            square(x, y, SIZE / NUM_TILES);
-        }
-    }
+          fill((i + j) % 2 * 255);
+          square(x, y, SIZE / NUM_TILES);
+      }
+  }
 }
 
 function draw() {}
